2b0c45ce7d192a46b49f5766af7c1e6b938a5106
Bernd Engels <engels@goldenacker.de>
2019-12-22 23:17:30 +0100

    bugfix

--- src/js/store/api.js
+++ src/js/store/api.js
@@ -1,29 +1,28 @@
 import { menuTypes } from "../inc/const";
 
-export const getEvents = (category = null) => {
+export const getEvents = function(category = null) {
 	const url = category ? '/api/spa/events/category/' + category : '/api/events';
 	return axiosCache
 		.get(url)
 		.then(response => {
 			return response.data;
 		})
-		.catch(err => {
+		.catch(function(err) {
 			alert(err);
 			return err;
 		});
 };
 
-export const getMenu = (type) => {
+export const getMenu = function(type) {
 	if (menuTypes.indexOf(type) === -1) {
-		alert('menu type: ' + type + ' not found!');
 		return null;
 	}
 	return axiosCache
 		.get('/api/spa/menu/' + type)
-		.then(response => {
+		.then(function(response) {
 			return response.data;
 		})
-		.catch(err => {
+		.catch(function(err) {
 			return err;
 		});
 };
@@ -31,37 +30,37 @@ export const getMenu = (type) => {
 export const getPage = (slug) => {
 	return axiosCache
 		.get('/api/spa/page/' + slug)
-		.then(response => {
+		.then(function(response) {
 			return response.data;
 		})
-		.catch(err => {
+		.catch(function(err) {
 			return err;
 		});
 };
 export const getMusicStyles = () => {
 	return axiosCache
 		.get('/api/spa/musicStyles')
-		.then(response => {
+		.then(function(response) {
 			return response.data;
 		})
-		.catch(err => {
+		.catch(function(err) {
 			return err;
 		});
 };
 export const getBandContactForm = () => {
 	return axiosCache
 		.get('/api/spa/contact/bands/fields')
-		.then(response => {
+		.then(function(response) {
 			return response.data;
 		})
-		.catch(err => {
+		.catch(function(err) {
 			return err;
 		});
 };
 export const sendBandContactForm = (data) => {
 	return axiosCache
 		.post('/api/spa/contact/bands/send', data)
-		.then(response => {
+		.then(function(response) {
 			if(200 == response.status) {
 				return response.data;
 			}
@@ -69,7 +68,7 @@ export const sendBandContactForm = (data) => {
 				return response.data;
 			}
 		})
-		.catch(err => {
+		.catch(function(err) {
 			if(422 == err.response.status) {
 				return err.response.data;
 			}
